b148ebccdca49cc0c63a66e5e8c201bed6d955cd
Dmitry Vladmirovich Vasiliev <otterinjection@protonmail.ch>
2019-05-16 12:34:11 +0200

    Add build to docs

---
+++ docs/precache-manifest.6976b1b2ece21ef2422599e98f78dbad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1545c0342a1dbf3a25cca2811190d955",
+    "url": "/bulk-renamer/index.html"
+  },
+  {
+    "revision": "824557fe38064d86f19d",
+    "url": "/bulk-renamer/static/css/main.97d3cefe.chunk.css"
+  },
+  {
+    "revision": "bb726d00baacd17fa7d4",
+    "url": "/bulk-renamer/static/js/2.027a225d.chunk.js"
+  },
+  {
+    "revision": "824557fe38064d86f19d",
+    "url": "/bulk-renamer/static/js/main.9a252d02.chunk.js"
+  },
+  {
+    "revision": "8814e4caf9aaa643c58a",
+    "url": "/bulk-renamer/static/js/runtime~main.f72929d8.js"
+  }
+]);
