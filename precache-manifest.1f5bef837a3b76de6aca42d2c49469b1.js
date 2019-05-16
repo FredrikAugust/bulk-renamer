58489ea7a3619c118dd55a99ad6f65169691a1c9
Dmitry Vladmirovich Vasiliev <otterinjection@protonmail.ch>
2019-05-16 13:01:54 +0200

    Updates

---
+++ precache-manifest.1f5bef837a3b76de6aca42d2c49469b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9883b58af20edc4d3d4227440b50f1c",
+    "url": "/bulk-renamer/index.html"
+  },
+  {
+    "revision": "ca4c5f76e6339e4cd858",
+    "url": "/bulk-renamer/static/css/main.97d3cefe.chunk.css"
+  },
+  {
+    "revision": "bb726d00baacd17fa7d4",
+    "url": "/bulk-renamer/static/js/2.027a225d.chunk.js"
+  },
+  {
+    "revision": "ca4c5f76e6339e4cd858",
+    "url": "/bulk-renamer/static/js/main.dd88762a.chunk.js"
+  },
+  {
+    "revision": "8814e4caf9aaa643c58a",
+    "url": "/bulk-renamer/static/js/runtime~main.f72929d8.js"
+  }
+]);
